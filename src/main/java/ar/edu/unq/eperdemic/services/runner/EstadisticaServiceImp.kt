3023484b64cc05d3a9883c4624ba977b178694b1
CristianGonzalez1980 <GitHub1980>
2020-05-16 03:00:31 -0300

    estadisticaServiceTest-Logica

---
+++ src/main/java/ar/edu/unq/eperdemic/services/runner/EstadisticaServiceImp.kt
@@ -0,0 +1,19 @@
+package ar.edu.unq.eperdemic.services.runner
+
+import ar.edu.unq.eperdemic.modelo.Especie
+import ar.edu.unq.eperdemic.modelo.ReporteDeContagios
+import ar.edu.unq.eperdemic.services.EstadisticasService
+
+class EstadisticaServiceImp() : EstadisticasService {
+    override fun especieLider(): Especie {
+        TODO("Not yet implemented")
+    }
+
+    override fun lideres(): List<Especie> {
+        TODO("Not yet implemented")
+    }
+
+    override fun reporteDeContagios(nombreUbicacion: String): ReporteDeContagios {
+        TODO("Not yet implemented")
+    }
+}
--- src/test/java/ar/edu/unq/eperdemic/utils/impl/DataServiceImp.kt
+++ src/test/java/ar/edu/unq/eperdemic/utils/impl/DataServiceImp.kt
@@ -1,4 +1,15 @@
 package ar.edu.unq.eperdemic.utils.impl
 
-class DataServiceImp {
+import ar.edu.unq.eperdemic.persistencia.dao.DataDAO
+import ar.edu.unq.eperdemic.services.runner.TransactionRunner
+import ar.edu.unq.eperdemic.utils.DataService
+
+class DataServiceImp(private val dataDAO: DataDAO) : DataService {
+    override fun crearSetDeDatosIniciales() {
+        TODO("Not yet implemented")
+    }
+
+    override fun eliminarTodo() {
+        TransactionRunner.runTrx { dataDAO.clear() }
+    }
 }
