0d47699d7ebe56e7f84cb504845dcc33737e481d
Matias <mati.nicolosi@gmail.com>
2020-06-05 19:40:00 -0300

    Arreglo de calculo en dar contagio y su probabilidad.

--- src/main/java/ar/edu/unq/eperdemic/modelo/StrategyVectores/StrategySuperClase.kt
+++ src/main/java/ar/edu/unq/eperdemic/modelo/StrategyVectores/StrategySuperClase.kt
@@ -7,13 +7,13 @@ import ar.edu.unq.eperdemic.modelo.Vector
 open class StrategySuperClase() {
 
     open fun darContagio(vectorInfectado: Vector, vectorAContagiar: Vector) {
-        val simulRandomEntre0y100 = 0 //Esto simula un random entre 0 y 100 que es el que determina si entra en el porcentaje
+        val simulRandomEntre1y100 = 1 //Esto simula un random entre 1 y 100 que es el que determina si entra en el porcentaje
         //de probabilidad o no, esta hecho de esta manera para que siempre entre en contagio y poder testear esos casos positivos de contagio.
         val enfermedades: MutableSet<Especie> = vectorInfectado.enfermedades
         for (e: Especie in enfermedades) {
             val factorContagio = e.owner?.capacidadContagio
             val porcentajeDeContagioExitoso = Random().giveRandom() + factorContagio!!
-            if ((simulRandomEntre0y100 <= porcentajeDeContagioExitoso) and (!vectorAContagiar.enfermedades.contains(e))) {
+            if ((simulRandomEntre1y100 <= porcentajeDeContagioExitoso) and (!vectorAContagiar.enfermedades.contains(e))) {
                 this.infectar(vectorAContagiar, e)
             }
         }
