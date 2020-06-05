58b96dbba890766371a3b97b3dd380d4e45eef2d
Matias <mati.nicolosi@gmail.com>
2020-06-05 17:08:49 -0300

    Arreglo del issue "darContagio".

--- src/main/java/ar/edu/unq/eperdemic/modelo/StrategyVectores/StrategySuperClase.kt
+++ src/main/java/ar/edu/unq/eperdemic/modelo/StrategyVectores/StrategySuperClase.kt
@@ -7,11 +7,13 @@ import ar.edu.unq.eperdemic.modelo.Vector
 open class StrategySuperClase() {
 
     open fun darContagio(vectorInfectado: Vector, vectorAContagiar: Vector) {
+        val simulRandomEntre0y100 = 0 //Esto simula un random entre 0 y 100 que es el que determina si entra en el porcentaje
+        //de probabilidad o no, esta hecho de esta manera para que siempre entre en contagio y poder testear esos casos positivos de contagio.
         val enfermedades: MutableSet<Especie> = vectorInfectado.enfermedades
         for (e: Especie in enfermedades) {
             val factorContagio = e.owner?.capacidadContagio
             val porcentajeDeContagioExitoso = Random().giveRandom() + factorContagio!!
-            if ((porcentajeDeContagioExitoso > 70) and (!vectorAContagiar.enfermedades.contains(e))) {
+            if ((simulRandomEntre0y100 <= porcentajeDeContagioExitoso) and (!vectorAContagiar.enfermedades.contains(e))) {
                 this.infectar(vectorAContagiar, e)
             }
         }
