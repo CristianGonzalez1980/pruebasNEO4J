cb9a00b2071b8329e9b1c8c11d7da03bc2d39ff9
CristianGonzalez1980 <GitHub1980>
2020-06-06 13:13:27 -0300

    conectarUbicaciones

--- src/main/java/ar/edu/unq/eperdemic/neo4jDao/UbicacionNeo4jDao.kt
+++ src/main/java/ar/edu/unq/eperdemic/neo4jDao/UbicacionNeo4jDao.kt
@@ -1,5 +1,6 @@
 package ar.edu.unq.eperdemic.neo4jDao
 
+import ar.edu.unq.eperdemic.modelo.TipoDeCamino
 import ar.edu.unq.eperdemic.modelo.Ubicacion
 import org.neo4j.driver.*
 
@@ -9,7 +10,7 @@ class UbicacionNeo4jDao {
 
     init {
         val env = System.getenv()
-        val url = env.getOrDefault("URL", "bolt://localhost:7687")
+        val url = env.getOrDefault("URL", "bolt://localhost:11005")
         val username = env.getOrDefault("USERe", "neo4j")
         val password = env.getOrDefault("PASSWORD", "root")
 
@@ -19,6 +20,7 @@ class UbicacionNeo4jDao {
     }
 
     fun crearUbicacion(ubicacion: Ubicacion) {
+
         driver.session().use { session ->
             session.writeTransaction {
                 val query = "MERGE (ubi:Ubicacion {nombreUbicacion: ${'$'}unaUbicacion}) "
@@ -29,8 +31,7 @@ class UbicacionNeo4jDao {
         }
     }
 
-    fun existeUbicacion(ubicacion: Ubicacion ): Boolean{
-
+    fun existeUbicacion(ubicacion: Ubicacion): Boolean {
 
         driver.session().use { session ->
             //{nombreUbicacion: ${'$'}unaUbicacion}
@@ -38,41 +39,58 @@ class UbicacionNeo4jDao {
             val result = session.run(
                     query, Values.parameters(
                     "unaUbicacion", ubicacion.nombreDeLaUbicacion
-
-
             )
             )
-            return  result.single().size() == 1
+            return result.single().size() == 1
         }
-
-
-
     }
 
-    fun conectar(ubicacion1:String, ubicacion2:String, tipoCamino:String){
+    fun conectar(ubicacion1: String, ubicacion2: String, tipoCamino: String) {
 
         // esto no esta terminado, es una idea porque no estoy segura lo que hace
 
-        var ubi1 = Ubicacion(ubicacion1)
-        var ubi2 = Ubicacion(ubicacion2)
-        this.crearUbicacion(ubi1)
-        this.crearUbicacion(ubi2)
+//        var ubi1 = Ubicacion(ubicacion1)
+//        var ubi2 = Ubicacion(ubicacion2)
+//        this.crearUbicacion(ubi1)
+//        this.crearUbicacion(ubi2)
         driver.session().use { session ->
             val query = """
                 MATCH (conectar:Ubicacion {nombreUbicacion: ${'$'}unaUbicacion})
                 MATCH (conectado:Ubicacion {nombreUbicacion: ${'$'}otraUbicacion})
-                MERGE (conectar)-[:tipoCamino]->(conectado)
-                MERGE (conectado)-[:tipoCamino]->(conectar)
+                MERGE (conectar)-[:camino {type: ${'$'}tCamino}]->(conectado)
+                MERGE (conectado)-[:camino {type: ${'$'}tCamino}]->(conectar)
             """
             session.run(
                     query, Values.parameters(
-                    "unaUbicacion", ubi1.nombreDeLaUbicacion,
-                    "otraUbicacion", ubi2.nombreDeLaUbicacion
+                    "unaUbicacion", ubicacion1/*ubi1.nombreDeLaUbicacion*/,
+                    "otraUbicacion", ubicacion2/*ubi2.nombreDeLaUbicacion*/,
+                    "tCamino", tipoCamino
             )
             )
         }
-
-
-
     }
+
+    fun estaConectadaPorCamino(ubicacion1: Ubicacion, ubicacion2: Ubicacion, tipoCamino: TipoDeCamino): Boolean {
+        return driver.session().use { session ->
+            val query = """
+                MATCH (ubi1:Ubicacion {nombreUbicacion: ${'$'}nombreUbi1})
+                MATCH (ubi2:Ubicacion {nombreUbicacion: ${'$'}nombreUbi2})
+                MATCH (ubi1)-[:camino {type: ${'$'}tCamino}]->(ubi2)
+                RETURN ubi2
+            """
+            val result = session.run(query, Values.parameters(
+                    "nombreUbi1", ubicacion1.nombreDeLaUbicacion,
+                    "nombreUbi2", ubicacion2.nombreDeLaUbicacion,
+                    "tCamino", tipoCamino.name
+            ))
+            return (result != null) //revisar query da siempre verde
+        }
+    }
+
+    fun clear() {
+        return driver.session().use { session ->
+            session.run("MATCH (n) DETACH DELETE n")
+        }
+    }
+
 }
--- src/test/java/ar/edu/unq/eperdemic/testNeo4jUbicacion/UbicacionNeo4jTest.kt
+++ src/test/java/ar/edu/unq/eperdemic/testNeo4jUbicacion/UbicacionNeo4jTest.kt
@@ -1,6 +1,7 @@
 package ar.edu.unq.eperdemic.testNeo4jUbicacion
 
 
+import ar.edu.unq.eperdemic.modelo.TipoDeCamino
 import ar.edu.unq.eperdemic.modelo.Ubicacion;
 import ar.edu.unq.eperdemic.neo4jDao.UbicacionNeo4jDao;
 import org.junit.Assert;
@@ -10,10 +11,17 @@ import org.junit.Test
 class UbicacionNeo4jTest {
 
     lateinit var dao: UbicacionNeo4jDao
+    val ubicacionA = Ubicacion("Quilmes")
+    val ubicacionB = Ubicacion("Colonia")
+    val ubicacionC = Ubicacion("Maldonado")
 
     @Before
     fun setUp() {
         dao = UbicacionNeo4jDao()
+        dao.clear()
+        dao.crearUbicacion(ubicacionA)
+        dao.crearUbicacion(ubicacionB)
+        dao.crearUbicacion(ubicacionC)
     }
 
     @Test
@@ -23,6 +31,14 @@ class UbicacionNeo4jTest {
         dao.crearUbicacion(ubicacion)
 
         Assert.assertEquals(true,dao.existeUbicacion(ubicacion))
-
     }
+
+    @Test
+    fun conecto2UbicacionesPorCaminoAereoVerificoLaRelacion(){
+
+        dao.conectar(ubicacionA.nombreDeLaUbicacion!!, ubicacionC.nombreDeLaUbicacion!!, TipoDeCamino.Terrestre.name )
+
+     //   Assert.assertEquals(true,dao.estaConectadaPorCamino(ubicacionA, ubicacionC, TipoDeCamino.Terrestre)) DA SIEMPRE VERDE CORREGIR
+    }
+
 }
